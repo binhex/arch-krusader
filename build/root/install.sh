7547c63f27dd66846b8b7111119e95f0e7a99c06
binhex <megalith01@gmail.com>
2021-07-26 10:51:18 +0100

    exclude overwrite of init.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,7 +3,18 @@
 # exit script if return code != 0
 set -e
 
-# note do NOT download build scripts - inherited from int script with envvars common defined
+# build scripts
+####
+
+# download build scripts from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /usr/local/bin (-n = do not overwrite
+# exsting files, required as init.sh written to via arch-int-gui)
+mv -n /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
 
 # detect image arch
 ####
